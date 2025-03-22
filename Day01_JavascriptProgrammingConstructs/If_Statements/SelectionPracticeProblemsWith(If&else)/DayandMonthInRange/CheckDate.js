c1d5c37cdc70d360857d9c7cbe4fb536f6020eca
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-03-22 11:19:11 +0530

    [Aman]Add. day and month in range.

---
+++ Day01_JavascriptProgrammingConstructs/If_Statements/SelectionPracticeProblemsWith(If&else)/DayandMonthInRange/CheckDate.js
@@ -0,0 +1,26 @@
+//WAP that takes day and month from command line and prints true if day of month is bw March 20 and june 20,false otherwise.
+let day = parseInt(process.argv[2]);
+let month = parseInt(process.argv[3]);
+
+console.log("Date - " + day + "/" + month);
+
+let isValid = false;
+if (month === 3) {
+    if (day >= 20 && day <= 31) {
+        isValid = true;
+    }
+} else if (month === 4) {
+    if (day >= 1 && day <= 30) {
+        isValid = true;
+    }
+} else if (month === 5) {
+    if (day >= 1 && day <= 31) {
+        isValid = true;
+    }
+} else if (month === 6) {
+    if (day >= 1 && day <= 20) {
+        isValid = true;
+    }
+}
+
+console.log(isValid);
