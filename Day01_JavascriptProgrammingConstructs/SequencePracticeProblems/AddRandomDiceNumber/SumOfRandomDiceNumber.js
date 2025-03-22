6425de10c49a5a1cd149da68e4503b6add413840
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-03-22 11:03:30 +0530

    [Aman] Add.  Sum of RandomDice Number.

---
+++ Day01_JavascriptProgrammingConstructs/SequencePracticeProblems/AddRandomDiceNumber/SumOfRandomDiceNumber.js
@@ -0,0 +1,4 @@
+//Add two Random Dice Number and print the Result.
+let dice1 = Math.floor(Math.random() * 10);
+let dice2 = Math.floor(Math.random() * 10);
+console.log("Dice 1:", dice1, "Dice 2:", dice2, "Sum:", dice1 + dice2);
