392c8ad43a28b49c5280b8d3c714db4555dbb3b3
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-03-22 11:36:19 +0530

    [Aman]Add. check prime number.

---
+++ Day01_JavascriptProgrammingConstructs/forLoopStatement/RepetiitonPraticeProblemsWithForLoop/CheckPrimeNumber/IsPrime.js
@@ -0,0 +1,17 @@
+//WAP that takes input and determines if the number is a prime. 
+
+let num = parseInt(process.argv[2]); // Command-line input
+let isPrime = true;
+
+if (num < 2) {
+    isPrime = false;
+} else {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+}
+
+console.log(num + (isPrime ? " is a Prime Number" : " is not a Prime Number"));
