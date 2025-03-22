ff172933b39bd0d9c5244ce6449b25914ef747ef
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-03-22 11:18:33 +0530

    [Aman]Add. Flip the coin.

---
+++ Day01_JavascriptProgrammingConstructs/If_Statements/SelectionPracticeProblemsWith(If&else)/CoinFlipHeadorTails/CoinFlip.js
@@ -0,0 +1,9 @@
+//WAP to simulate a coin flip and print out "Heads" or "Tails" accordingly.
+
+let coinFlip = Math.random();
+
+if (coinFlip < 0.5) {
+    console.log("Heads");
+} else {
+    console.log("Tails");
+}
