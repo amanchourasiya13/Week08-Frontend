1f532cbb4919676ce6e01783a38f6a9004786e57
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-03-22 11:31:48 +0530

    [Aman]Add. read num and sisplay place unit.

---
+++ Day01_JavascriptProgrammingConstructs/CaseStatements/SelectionPracticeProblemsWithCaseStatements/ReadNumAndDisplayPlaceUnit/PlaceValueUnit.js
@@ -0,0 +1,26 @@
+//Read a number 1,10,100,10000,etc and display unit ,ten,hundred,etc.
+
+let num = parseInt(process.argv[2]); // Command-line input
+
+switch (num) {
+    case 1:
+        console.log("Unit");
+        break;
+    case 10:
+        console.log("Ten");
+        break;
+    case 100:
+        console.log("Hundred");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    case 10000:
+        console.log("Ten Thousand");
+        break;
+    case 100000:
+        console.log("Lakh");
+        break;
+    default:
+        console.log("Invalid Input! Enter 1, 10, 100, 1000, etc.");
+}
